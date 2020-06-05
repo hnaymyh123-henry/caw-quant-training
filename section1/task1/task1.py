6a15dc9f57c4a808effb0246dd3ef6a8de189d17
Yuanheng Ma <ym2752@columbia.edu>
2020-06-05 13:12:35 -0400

    Modify function

--- section1/task1/task1.py
+++ section1/task1/task1.py
@@ -2,42 +2,43 @@ import pandas as pd
 import json
 import requests
 import time
-a={'fsym': 'BTC', 'tsym': 'USDT', 'limit': 1680, 'toTs': '1585713600', 'e': 'binance'}
-r = requests.get('https://min-api.cryptocompare.com/data/v2/histohour', params=a)
-r2 = json.loads(r.text)
-r3 = r2['Data']['Data']
-df = pd.DataFrame(r3)
-data = df[['close', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'time']]
-#print (data.head())
-t = data.loc[0, 'time']
-#print (t)
 
-while t>1490932800:
-    t = data.iloc[0, 6]
-    #print (t)
-    t2 = t-3600
-    #print (t2)
-    a={'fsym': 'BTC', 'tsym': 'USDT', 'limit': 1680, 'toTs': t2, 'e': 'binance'}
-    r = requests.get('https://min-api.cryptocompare.com/data/v2/histohour', params=a)
-    r2 = json.loads(r.text)
-    r3 = r2['Data']['Data']
-    df = pd.DataFrame(r3)
-    df = df[['close', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'time']]
-    data = pd.concat([df, data], axis=0)
+def get_candle(fsym='ETH', tsym='BTC', freq="1h", start_time="2018-01-01 00:00:00", end_time="2018-06-01 00:00:00", exchange='binance'):
     
+    time1 = time.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    end_time = time.mktime(time1)
+    end_time_real = end_time
+    time2 = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    start_time = time.mktime(time2)
 
-data1 = data[data['time']>=1491019200]
+    while end_time > start_time: 
+      a={'fsym': fsym, 'tsym': tsym, 'limit': 2000, 'toTs': end_time, 'e': exchange}
+      r = requests.get('https://min-api.cryptocompare.com/data/v2/histohour', params=a)
+      r2 = json.loads(r.text)
+      r3 = r2['Data']['Data']
+      df = pd.DataFrame(r3)
+      df = df[['close', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'time']]
+      if end_time == end_time_real:
+        data = df
+      else:
+        data = pd.concat([df, data], axis=0)
+      time3 = data.iloc[0, 6]
+      end_time = time3-3600
+    
+    data1 = data[data['time']>=start_time]
 
-date = [ ]
-for i in data1['time']:  
-    time_obj = time.localtime(i)      
-    i=time.strftime('%Y-%m-%d %H:%M:%S',time_obj)
-    date.append(i)
-data1[['time']] = date
+    data1['time'] = data1['time'].apply(lambda x : time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(x)))
 
-data1['volume'] = data1['volumeto']-data1['volumefrom']
-data1['basevolume']=data1['volumefrom']
-data2 = data1[['close', 'high', 'low', 'open', 'volume', 'basevolume', 'time']]
+    data1['volume'] = data1['volumeto']-data1['volumefrom']
+    data1['basevolume']=data1['volumefrom']
+    data2 = data1[['close', 'high', 'low', 'open', 'volume', 'basevolume', 'time']]
 
-print (data2.head())
-data2.to_csv('task1')
+    data2 = data2.set_index(['time'])
+    
+    return data2.to_csv('task1')
+  
+
+
+get_candle(fsym='ETH', tsym='BTC', freq="1h", start_time="2018-01-01 00:00:00", end_time="2018-06-01 00:00:00", exchange='binance')
+
+    
